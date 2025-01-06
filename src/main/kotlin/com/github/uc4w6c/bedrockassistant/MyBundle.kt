406252faf9192aca42ee638d2463ae1a91af7b13
GitHub Action <action@github.com>
2025-01-06 04:42:42 +0000

    Template cleanup

--- src/main/kotlin/com/github/uc4w6c/bedrockassistant/MyBundle.kt
+++ src/main/kotlin/com/github/uc4w6c/bedrockassistant/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.uc4w6c.bedrockassistant
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/uc4w6c/bedrockassistant/listeners/MyApplicationActivationListener.kt
+++ src/main/kotlin/com/github/uc4w6c/bedrockassistant/listeners/MyApplicationActivationListener.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.uc4w6c.bedrockassistant.listeners
 
 import com.intellij.openapi.application.ApplicationActivationListener
 import com.intellij.openapi.diagnostic.thisLogger
